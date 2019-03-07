7dbd9906582d83bd832fb0d359a43a99879ad7ff
Jimmy <642249018@qq.com>
2019-03-07 17:02:23 +0800

    'add-city-router'

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="head-wrapper">
+    <router-link to="/">
+      <div class="iconfont back-left">&#xe624;</div>
+    </router-link>
+    <h3 class="head-title">城市选择</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .head-wrapper
+    width: 100%
+    height: .86rem
+    line-height: .86rem
+    text-align: center
+    color: #fff
+    background: $bgColor
+    .back-left
+      float: left
+      width: .64rem
+      font-size: .36rem
+      font-weight: bold
+      color: #fff
+    .head-title
+      font-size: .32rem
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,9 +5,11 @@
         </div>
         <div class="header-input">
           <span class="iconfont">&#xe632;</span> 输入城市/景点/游玩主题</div>
-        <div class="header-right">{{this.city}}
-          <span class="iconfont arrow-icon">&#xe6aa;</span>
-        </div>
+        <router-link to="/city">
+          <div class="header-right">{{this.city}}
+            <span class="iconfont arrow-icon">&#xe6aa;</span>
+          </div>
+        </router-link>
     </div>
 </template>
 
@@ -49,6 +51,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.06rem
 </style>
